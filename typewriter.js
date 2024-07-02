b05b88a1a9eec3d502feb8103b2eddcdeb4daeb7
Kyle McParland <kylemcparland@gmail.com>
2024-07-01 20:39:30 -0400

    edited syntax

--- typewriter.js
+++ typewriter.js
@@ -3,15 +3,15 @@ const sentence = "hello there from lighthouse labs";
 let delay = 0;
 
 for (const char of sentence) {
-  delay += 50;
-
   setTimeout(() => {
     process.stdout.write(char);
   }, delay);
 
-  if (delay === 50) {
+  if (delay === 0) {
     setTimeout(() => {
       process.stdout.write('\n');
     }, 1650);
   }
+  
+  delay += 50;
 }
